db72bd640d39b5885a09ba866cc10e132e7f31a0
gantrol <gantrol.h@qq.com>
2023-01-27 17:34:05 +0800

    release: v0.1 simply allow edit and copy

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# 简单粗暴地编辑与复制网页
+
+有的网站已经能绕过 simple allow copy 这类插件，那就 simple allow 编辑。
---
+++ background.js
@@ -0,0 +1,28 @@
+const on = 'ON';
+const pageEditable = (isEditable) => {
+  // 允许编辑，顺带允许复制粘贴
+  document.body.contentEditable = isEditable;
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  // 由于浏览器限制，不能访问这些开头的链接
+  if(!(tab.url.includes("chrome://") || tab.url.includes("edge://"))) {
+    let text = await chrome.action.getBadgeText({tabId: tab.id});
+    text = text ? text : '';
+
+    let isEditable = text === on;
+    if (isEditable) {
+      text = ''
+    } else {
+      text = on;
+    }
+    isEditable = !isEditable;
+
+    chrome.action.setBadgeText({tabId: tab.id, text: text}); 
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: pageEditable,
+      args: [isEditable]
+    });
+  }
+});

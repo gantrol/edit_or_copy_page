202d1a00909dc02801c0333f8577d766f27ebe48
gantrol <gantrol.h@qq.com>
2023-01-31 19:31:21 +0800

    feat: 添加无权限是发通知提醒的功能

--- background.js
+++ background.js
@@ -4,10 +4,22 @@ const pageEditable = (isEditable) => {
   document.body.contentEditable = isEditable;
 }
 
+const ban_list = ["chrome://", "edge://", "chrome.google.com/webstore", "microsoftedge.microsoft.com/addons/"]
+
 chrome.action.onClicked.addListener(async (tab) => {
   // 由于浏览器限制，不能访问这些开头的链接
-  if(!(tab.url.includes("chrome://") || tab.url.includes("edge://"))) {
-    let text = await chrome.action.getBadgeText({tabId: tab.id});
+  const check_list = ban_list.map(str => tab.url.includes(str));
+  const check_result = check_list.reduce((prev, curr) => prev || curr);
+  if (check_result) {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icon/v1.2@4x.png',
+      title: chrome.i18n.getMessage("banTitle"),
+      message: chrome.i18n.getMessage("banMessage"),
+      priority: 0
+    })
+  } else {
+    let text = await chrome.action.getBadgeText({ tabId: tab.id });
     text = text ? text : '';
 
     let isEditable = text === on;
@@ -18,11 +30,11 @@ chrome.action.onClicked.addListener(async (tab) => {
     }
     isEditable = !isEditable;
 
-    chrome.action.setBadgeText({tabId: tab.id, text: text}); 
+    chrome.action.setBadgeText({ tabId: tab.id, text: text });
     chrome.scripting.executeScript({
-      target: { 
+      target: {
         tabId: tab.id,
-      allFrames: true,
+        allFrames: true,
 
       },
       function: pageEditable,
